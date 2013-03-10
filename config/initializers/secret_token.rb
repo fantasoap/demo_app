2eaba41070a43f4a3bda1a8a283bb9592b4c724f
Mark Vorhoff <fantasoap@hotmail.com>
2013-03-09 21:06:58 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '77da49f54ff5115da09e2a97390a183d3904c0324566102e132399b88bc85cba11ed19a030a3f96185c3b2927202b281369bf5ec8fb9b05c8f0f33aa3c41c198'
